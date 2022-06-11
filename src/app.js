5cd6a047f065d0154e2742f3c79ad0ccbd288584
Abbas Alkaabi <abbasalkabbi2003@gmail.com>
2022-06-11 11:40:34 +0300

    Add -> typing

--- src/app.js
+++ src/app.js
@@ -13,3 +13,61 @@ function ShowNav(){
    
 }
 // add function to Nav bar  END
+// typing 
+
+class TypingText {
+    constructor(TextElement,words,wait=300){
+        this.TextElement=TextElement;
+        this.words=words;
+        this.wait=parseInt(wait,10);
+        this.tex='';
+        this.wordindex=0;
+        this.typing();
+        this.isDeleting;
+    }
+    typing(){
+        const current=this.wordindex % this.words.length;
+        // get Fulltext 
+        const fulltext=this.words[current]
+        // check deleting 
+        if(this.isDeleting){
+            // remove char
+            this.tex=fulltext.substring(0,this.tex.length -1)
+        }else{
+             // add char
+            this.tex=fulltext.substring(0,this.tex.length +1)
+        }
+        // Insert txt into element
+        this.TextElement.innerHTML = `<span class="txt">${this.tex}</span>`;
+        // Initial Type Speed
+        let typeSpeed = 300;
+        if(this.isDeleting){
+            typeSpeed/=2;
+        }
+        // If word is complete
+        if(!this.isDeleting && this.tex===fulltext){
+            // Make pause at end
+            typeSpeed = this.wait;
+            this.isDeleting=true;
+        }else if(this.isDeleting && this.tex === '') {
+            this.isDeleting=false;
+            // move to next text
+            this.wordindex++;
+             // Pause before start typing
+            typeSpeed = 500;
+        }
+        setTimeout(()=>this.typing(),typeSpeed)
+    }
+}
+
+// Init On DOM Load
+document.addEventListener('DOMContentLoaded', init);
+
+// Init App
+function init() {
+    const txtElement = document.querySelector('.dynamic-text');
+    const words = JSON.parse(txtElement.getAttribute('data-words'));
+    const wait = txtElement.getAttribute('data-wait');
+  // Init TypeWriter
+    new TypingText(txtElement, words, wait);
+}
